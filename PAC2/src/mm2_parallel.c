036b19b4bb5b61c266e251aa15f47843f8593d85
AleixMT <aleixaretra@gmail.com>
2023-04-11 19:42:04 +0200

    UPDATED: C code

--- PAC2/src/mm2_parallel.c
+++ PAC2/src/mm2_parallel.c
@@ -1,31 +1,30 @@
-
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <memory.h>
-#include <malloc.h>
 
-#define SIZE 1000
+#define SIZE 800
 
-int main(int argc, char **argv) {
+int main() {
 
-  float matrixa[SIZE][SIZE], matrixb[SIZE][SIZE], mresult[SIZE][SIZE];
-  int i,j,k;
+    float matrixa[SIZE][SIZE], matrixb[SIZE][SIZE], mresult[SIZE][SIZE];
+    int i, j, k;
 
+    /* Initialize the Matrix arrays */
     #pragma omp parallel for
-  /* Initialize the Matrix arrays */
-  for ( i=0; i<SIZE*SIZE; i++ ){
-    mresult[0][i] = 0.0;
-    matrixa[0][i] = matrixb[0][i] = rand()*(float)1.1; }
+    for (i = 0; i < SIZE * SIZE; i++)
+    {
+        mresult[0][i] = 0.0;
+        matrixa[0][i] = matrixb[0][i] = rand() * (float) 1.1;
+    }
 
-#pragma omp parallel for
     /* Matrix-Matrix multiply */
-  for (k=0;k<SIZE;k++)
-   for(j=0;j<SIZE;j++)
-    for(i=0;i<SIZE;i++)
-      mresult[i][j]=mresult[i][j] + matrixa[i][k]*matrixb[k][j];
-
-  exit(0);
+    #pragma omp parallel shared(mresult, matrixa, matrixb) private(k)
+    {
+        #pragma omp for
+        for (k = 0; k < SIZE; k++) {
+            for (j = 0; j < SIZE; j++) {
+                for (i = 0; i < SIZE; i++) {
+                    mresult[i][j] = mresult[i][j] + matrixa[i][k] * matrixb[k][j];
+                }
+            }
+        }
+    }
 }
