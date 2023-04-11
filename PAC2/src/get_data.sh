fe1cfd2141b1e37d3e272938ea4644368dc44230
AleixMT <aleixaretra@gmail.com>
2023-04-11 17:11:39 +0200

    UPDATED: make file for study 3

---
+++ PAC2/src/get_data.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+study()
+{
+  DIR=$(dirname "$0")
+  if [ ! -f "${DIR}/$1" ]; then
+    gcc "${DIR}/$1.c" –o "${DIR}/$1"
+  fi
+
+  for i in 1 2 3 4; do
+    echo Using argument $i
+    cat << EOF > "${DIR}/job$i.sge"
+#!/bin/bash
+#$ -cwd
+#$ -S /bin/bash
+#$ -N omp
+#$ -o omp.out.$JOB_ID
+#$ -e omp.out.$JOB_ID
+#$ -pe openmp $i
+
+export OMP_NUM_THREADS=$NSLOTS
+{ time ./$1 2> /dev/null; } 2>&1 | grep -Eo ^real$'\t'[0-9]+m[0-9]+.[0-9]+s | cut -d $'\t' -f2 >> $1_${i}_data.txt
+EOF
+    for _ in {1..10}; do
+      echo Launching job with arg $i
+      qsub "${DIR}/job$i.sge"
+    done
+
+  done
+
+}
+
+study mm
+study mm2
